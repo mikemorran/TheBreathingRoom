b4d6d963ceddf8704c4c8b336bb73b089561340d
Michael Morran <michaelmorran@Michaels-MacBook-Pro-6.local>
2021-03-19 19:51:31 -0500

    initial commit

--- index.js
+++ index.js
@@ -10,12 +10,15 @@ server.listen(port, () => {
 });
 
 let mqtt = require('mqtt');
+const { stringify } = require('querystring');
 let client  = mqtt.connect('mqtt://192.168.0.14:1883');
  
 client.on('connect', function () {
   client.subscribe('presence', function (err) {
     if (!err) {
       client.publish('presence', 'Hello mqtt');
+      client.publish('zigbee2mqtt/0x0017880109ceac0b/set', '{"color":{"h":0,"s":100,"b":0}}');
+      client.publish('zigbee2mqtt/0x0017880109ceafc8/set', '{"color":{"h":0,"s":100,"b":0}}');
     }
   });
 });
@@ -34,10 +37,46 @@ io.sockets.on('connection', function(socket) {
     
     socket.on('button', function() {
         console.log('button message received');
-        client.publish('zigbee2mqtt/Switch1/set', '{"state": "TOGGLE"}', (err) => {
+        client.publish('zigbee2mqtt/0x0017880109ceac0b/set', '{"color":{"h":45,"s":100,"b":100}, "transition":1}', (err) => {
             console.log(err);
         });
+        client.publish('zigbee2mqtt/0x0017880109ceafc8/set', '{"color":{"h":45,"s":100,"b":100}, "transition":1}', (err) => {
+          console.log(err);
+      });
     });
+
+    // socket.on('soundLvl', (soundData) => {
+    //   // console.log(soundData);
+    //   if (soundData) {
+    //     let brightnessOutput = JSON.stringify(soundData);
+    //     console.log(brightnessOutput);
+    //     client.publish('zigbee2mqtt/0x0017880109ceac0b/set', '{"color":{"h":45,"s":100,"b":'+ brightnessOutput +'}}', (err) => {
+    //       console.log(err);
+    //     });
+    //     client.publish('zigbee2mqtt/0x0017880109ceafc8/set', '{"color":{"h":45,"s":100,"b":'+ brightnessOutput +'}}', (err) => {
+    //       console.log(err);
+    //     });
+    //   }
+    // })
+
+    socket.on('inhale', () => {
+      console.log('inhaling');
+      client.publish('zigbee2mqtt/0x0017880109ceac0b/set', '{"brightness_move" : 250}', (err) => {
+        console.log(err);
+      });
+      client.publish('zigbee2mqtt/0x0017880109ceafc8/set', '{"brightness_move" : 250}', (err) => {
+        console.log(err);
+      });
+    })
+    socket.on('exhale', () => {
+      console.log('exhaling');
+      client.publish('zigbee2mqtt/0x0017880109ceac0b/set', '{"brightness_move" : -150}', (err) => {
+        console.log(err);
+      });
+      client.publish('zigbee2mqtt/0x0017880109ceafc8/set', '{"brightness_move" : -150}', (err) => {
+        console.log(err);
+      });
+    })
 });
 
 // client.publish('zigbee2mqtt/Switch1/set', '{"state": "TOGGLE"}');
--- public/app.js
+++ public/app.js
@@ -1,17 +1,23 @@
 console.log('hello world');
 
-let poseNet;
-let pose;
-let video;
 let videoWidth = 0;
 let videoHeight = 0;
 let Canvas;
+let mic;
+let breathing = false;
+let exhaleSent = true;
+let threshold = 0.03;
+let audio = false;
+let actionSent = true;
 let buttonPressed = true;
 
 window.addEventListener('load', () => {
     document.getElementById('button').addEventListener('click', () => {
         console.log('button pushed');
         socket.emit('button', buttonPressed);
+        userStartAudio();
+        soundOut();
+        audio = true;
     });
 });
 
@@ -21,64 +27,25 @@ function setup() {
     console.log(videoWidth, videoHeight);
     Canvas = createCanvas(videoWidth, videoHeight);
     Canvas.parent('videoCaptureDiv');
-
-    video = createCapture(VIDEO, () => {
-        console.log('user video captured');
-    });
-    video.hide();
-
-    // Initialize poseNet
-    poseNet = ml5.poseNet(video, () => {
-        console.log('Model Ready');
-    });
-    poseNet.on('pose', (poses) => {
-        if (poses.length > 0) {
-            pose = poses[0].pose;
-        }
-    });
+    mic = new p5.AudioIn();
+    mic.start();
 }
 
-let hasClapped = false;
-let counter = 0;
-let limitation = 8;
-let boundary = 550;
-
 function draw() {
-    if (pose) {
-        image(video, 0, 0, videoWidth, videoHeight);
-        let handDistance = dist(pose.rightWrist.x, pose.rightWrist.y, pose.leftWrist.x, pose.rightWrist.y);
-        // console.log(handDistance);
-        // let currentTime = millis();
-        // let adjustedTime = floor(currentTime);
-        // console.log(counter, handDistance);
-        // console.log(handDistance);
-        // let dividend = floor(handDistance/limitation);
-        // if (handDistance <= boundary && counter % dividend == 0) {
-        //     socket.emit('button', buttonPressed);
-        //     console.log('button pushed');
-        // }
-        // if (handDistance <= 100 && counter % 6 == 0) {
-        //     socket.emit('button', buttonPressed);
-        //     console.log('button pushed');
-        // }
-        if (handDistance <= 50 && !hasClapped) {
-            socket.emit('button', buttonPressed);
-            console.log('button pushed');
-            hasClapped = true;
+        let soundLvl = mic.getLevel();
+        console.log(soundLvl);
+        // let soundLvlOutput = Math.floor(map(soundLvl, 0, 0.5, 0, 255, true));
+        // socket.emit('soundLvl', soundLvlOutput);
+        if (soundLvl < threshold && !exhaleSent) {
+            //send exhale, set exhaleSent to true
+            socket.emit('exhale', actionSent);
+            exhaleSent = true;
         }
-        if (handDistance >= 150 && hasClapped) {
-            hasClapped = false;
-            console.log('reset');
+        if (soundLvl > threshold && exhaleSent) {
+            //send inhale, set exhaleSent to false
+            socket.emit('inhale', actionSent);
+            exhaleSent = false;
         }
-        // ellipse(pose.rightWrist.x, pose.rightWrist.y, 50);
-        // ellipse(pose.leftWrist.x, pose.leftWrist.y, 50);
-        // for (i = 0; i < pose.keypoints.length; i++) {
-        //     let x = pose.keypoints[i].position.x;
-        //     let y = pose.keypoints[i].position.y;
-        //     ellipse(x, y, 24);
-        // }
-    }
-    counter++;
 }
 
 function windowResized() {
